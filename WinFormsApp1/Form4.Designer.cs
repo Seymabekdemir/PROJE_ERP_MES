30cfd772e891e5996f44fe2b28722a327b3dcc35
Şeyma Bekdemir <seymabekdemir03@gmail.com>
2025-07-25 11:28:11 +0300

    Cihaz görüntüleme sayfası (form4) düzenlendi ve oluşturulduğu işlev için gerekli bütün companantları eklendi ve test edildi.

--- WinFormsApp1/Form4.Designer.cs
+++ WinFormsApp1/Form4.Designer.cs
@@ -34,12 +34,14 @@
             textBoxGoruntuleCihazId = new TextBox();
             textBoxGoruntuleCihazAdi = new TextBox();
             textBoxGoruntuleCihazTuru = new TextBox();
-            textBoxGoruntuleCihazAciklaması = new TextBox();
+            textBoxGoruntuleCihazAciklamasi = new TextBox();
             label1 = new Label();
             label3 = new Label();
             label4 = new Label();
             label5 = new Label();
+            panel1 = new Panel();
             ((System.ComponentModel.ISupportInitialize)dataGridViewCihazGoruntule).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // txtAramaCihazGoruntule
@@ -48,20 +50,22 @@
             txtAramaCihazGoruntule.Name = "txtAramaCihazGoruntule";
             txtAramaCihazGoruntule.Size = new Size(388, 23);
             txtAramaCihazGoruntule.TabIndex = 0;
+            txtAramaCihazGoruntule.TextChanged += txtAramaCihazGoruntule_TextChanged;
             // 
             // dataGridViewCihazGoruntule
             // 
             dataGridViewCihazGoruntule.BackgroundColor = SystemColors.Info;
+            dataGridViewCihazGoruntule.BorderStyle = BorderStyle.Fixed3D;
             dataGridViewCihazGoruntule.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridViewCihazGoruntule.Location = new Point(12, 41);
             dataGridViewCihazGoruntule.Name = "dataGridViewCihazGoruntule";
-            dataGridViewCihazGoruntule.Size = new Size(388, 356);
+            dataGridViewCihazGoruntule.Size = new Size(431, 356);
             dataGridViewCihazGoruntule.TabIndex = 1;
             dataGridViewCihazGoruntule.CellContentClick += dataGridViewCihazGoruntule_CellContentClick;
             // 
             // textBoxGoruntuleCihazId
             // 
-            textBoxGoruntuleCihazId.Location = new Point(606, 41);
+            textBoxGoruntuleCihazId.Location = new Point(169, 10);
             textBoxGoruntuleCihazId.Multiline = true;
             textBoxGoruntuleCihazId.Name = "textBoxGoruntuleCihazId";
             textBoxGoruntuleCihazId.Size = new Size(189, 36);
@@ -69,7 +73,7 @@
             // 
             // textBoxGoruntuleCihazAdi
             // 
-            textBoxGoruntuleCihazAdi.Location = new Point(606, 93);
+            textBoxGoruntuleCihazAdi.Location = new Point(169, 62);
             textBoxGoruntuleCihazAdi.Multiline = true;
             textBoxGoruntuleCihazAdi.Name = "textBoxGoruntuleCihazAdi";
             textBoxGoruntuleCihazAdi.Size = new Size(189, 36);
@@ -78,27 +82,27 @@
             // 
             // textBoxGoruntuleCihazTuru
             // 
-            textBoxGoruntuleCihazTuru.Location = new Point(606, 147);
+            textBoxGoruntuleCihazTuru.Location = new Point(169, 116);
             textBoxGoruntuleCihazTuru.Multiline = true;
             textBoxGoruntuleCihazTuru.Name = "textBoxGoruntuleCihazTuru";
             textBoxGoruntuleCihazTuru.Size = new Size(189, 36);
             textBoxGoruntuleCihazTuru.TabIndex = 4;
             // 
-            // textBoxGoruntuleCihazAciklaması
+            // textBoxGoruntuleCihazAciklamasi
             // 
-            textBoxGoruntuleCihazAciklaması.Location = new Point(606, 203);
-            textBoxGoruntuleCihazAciklaması.Multiline = true;
-            textBoxGoruntuleCihazAciklaması.Name = "textBoxGoruntuleCihazAciklaması";
-            textBoxGoruntuleCihazAciklaması.Size = new Size(189, 101);
-            textBoxGoruntuleCihazAciklaması.TabIndex = 5;
+            textBoxGoruntuleCihazAciklamasi.Location = new Point(169, 172);
+            textBoxGoruntuleCihazAciklamasi.Multiline = true;
+            textBoxGoruntuleCihazAciklamasi.Name = "textBoxGoruntuleCihazAciklamasi";
+            textBoxGoruntuleCihazAciklamasi.Size = new Size(189, 101);
+            textBoxGoruntuleCihazAciklamasi.TabIndex = 5;
             // 
             // label1
             // 
-            label1.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label1.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold);
             label1.ImageAlign = ContentAlignment.BottomCenter;
-            label1.Location = new Point(428, 41);
+            label1.Location = new Point(6, 10);
             label1.Name = "label1";
-            label1.Size = new Size(165, 29);
+            label1.Size = new Size(150, 29);
             label1.TabIndex = 6;
             label1.Text = "Cihaz Id:";
             label1.TextAlign = ContentAlignment.MiddleLeft;
@@ -106,11 +110,11 @@
             // 
             // label3
             // 
-            label3.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label3.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold);
             label3.ImageAlign = ContentAlignment.BottomCenter;
-            label3.Location = new Point(428, 93);
+            label3.Location = new Point(6, 62);
             label3.Name = "label3";
-            label3.Size = new Size(134, 29);
+            label3.Size = new Size(119, 29);
             label3.TabIndex = 8;
             label3.Text = "Cihaz Adı:";
             label3.TextAlign = ContentAlignment.MiddleLeft;
@@ -118,25 +122,41 @@
             // 
             // label4
             // 
-            label4.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label4.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold);
             label4.ImageAlign = ContentAlignment.BottomCenter;
-            label4.Location = new Point(428, 147);
+            label4.Location = new Point(6, 116);
             label4.Name = "label4";
-            label4.Size = new Size(158, 29);
+            label4.Size = new Size(143, 29);
             label4.TabIndex = 9;
             label4.Text = "Cihaz Türü:";
             label4.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // label5
             // 
-            label5.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label5.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold);
             label5.ImageAlign = ContentAlignment.BottomCenter;
-            label5.Location = new Point(428, 203);
+            label5.Location = new Point(6, 172);
             label5.Name = "label5";
-            label5.Size = new Size(172, 29);
+            label5.Size = new Size(157, 29);
             label5.TabIndex = 10;
             label5.Text = "Cihaz Açıklaması:";
             label5.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(textBoxGoruntuleCihazAciklamasi);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(textBoxGoruntuleCihazTuru);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(textBoxGoruntuleCihazAdi);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(textBoxGoruntuleCihazId);
+            panel1.Location = new Point(449, 41);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(369, 284);
+            panel1.TabIndex = 11;
             // 
             // Form4
             // 
@@ -144,14 +164,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.AliceBlue;
             ClientSize = new Size(830, 446);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label1);
-            Controls.Add(textBoxGoruntuleCihazAciklaması);
-            Controls.Add(textBoxGoruntuleCihazTuru);
-            Controls.Add(textBoxGoruntuleCihazAdi);
-            Controls.Add(textBoxGoruntuleCihazId);
+            Controls.Add(panel1);
             Controls.Add(dataGridViewCihazGoruntule);
             Controls.Add(txtAramaCihazGoruntule);
             Icon = (Icon)resources.GetObject("$this.Icon");
@@ -159,6 +172,8 @@
             Text = "Cihazlar görüntüle";
             Load += Form4_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridViewCihazGoruntule).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -170,10 +185,11 @@
         private TextBox textBoxGoruntuleCihazId;
         private TextBox textBoxGoruntuleCihazAdi;
         private TextBox textBoxGoruntuleCihazTuru;
-        private TextBox textBoxGoruntuleCihazAciklaması;
+        private TextBox textBoxGoruntuleCihazAciklamasi;
         private Label label1;
         private Label label3;
         private Label label4;
         private Label label5;
+        private Panel panel1;
     }
 }
--- WinFormsApp1/Form4.cs
+++ WinFormsApp1/Form4.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,18 +25,7 @@ namespace ERP_MES
 
         private void Form4_Load(object sender, EventArgs e)
         {
-            using (SqlConnection baglanti = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MESDb;Integrated Security=True;Encrypt=True"))
-            {
-                baglanti.Open();
-                SqlCommand command = new SqlCommand("SELECT CihazAdı, CihazTuru FROM Cihazlar", baglanti);
-                SqlDataAdapter adapter = new SqlDataAdapter(command);
-                DataTable dataTable = new DataTable();
-                adapter.Fill(dataTable);
-                dataGridViewCihazGoruntule.DataSource = dataTable;
-                dataGridViewCihazGoruntule.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-                dataGridViewCihazGoruntule.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-                dataGridViewCihazGoruntule.ReadOnly = true;
-            }
+            CihazAramaYap();    
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -64,9 +54,67 @@ namespace ERP_MES
 
         private void dataGridViewCihazGoruntule_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            dataGridViewCihazGoruntule.CellClick += dataGridViewCihazGoruntule_CellClick;
+        }
+
+            public void dataGridViewCihazGoruntule_CellClick(object sender, DataGridViewCellEventArgs e)
+            {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                DataGridViewRow row = dataGridViewCihazGoruntule.Rows[e.RowIndex];
+                // Seçilen satırdaki hücre değerlerini al
+                string cihazId = row.Cells["CihazId"].Value.ToString();
+                string cihazAdi = row.Cells["CihazAdı"].Value.ToString();
+                string cihazAciklaması = row.Cells["CihazAçıklaması"].Value.ToString();
+                string cihazTuru = row.Cells["CihazTuru"].Value.ToString();
+
+                // İlgili hücreleri textboxlara yaz
+                textBoxGoruntuleCihazId.Text = row.Cells["CihazId"].Value.ToString();
+                textBoxGoruntuleCihazAdi.Text = row.Cells["CihazAdı"].Value.ToString();
+                textBoxGoruntuleCihazTuru.Text = row.Cells["CihazTuru"].Value.ToString();
+                textBoxGoruntuleCihazAciklamasi.Text = row.Cells["CihazAçıklaması"].Value.ToString();
+            }
 
         }
 
-       
+        private void CihazAramaYap()
+        {
+            string filtre = txtAramaCihazGoruntule.Text.Trim();
+
+            using (SqlConnection baglanti = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MESDb;Integrated Security=True;Encrypt=True"))
+            {
+               
+                baglanti.Open();
+                string query;
+
+                    if (string.IsNullOrEmpty(filtre))
+                    {
+                        query = "SELECT * FROM Cihazlar";
+                    }
+                    else
+                    {
+                        query = "SELECT * FROM Cihazlar WHERE CihazAdı LIKE @arama OR CihazTuru LIKE @arama";
+                    }
+                    SqlCommand command = new SqlCommand(query, baglanti);
+                    if (!string.IsNullOrEmpty(filtre))
+                    {
+                        command.Parameters.AddWithValue("@arama", "%" + filtre + "%");
+                    }
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+
+                    dataGridViewCihazGoruntule.DataSource = dataTable;
+                    dataGridViewCihazGoruntule.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                    dataGridViewCihazGoruntule.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                    dataGridViewCihazGoruntule.ReadOnly = true;
+                }
+            }
+        
+        private void txtAramaCihazGoruntule_TextChanged(object sender, EventArgs e)
+        {
+            CihazAramaYap();
+        }
     }
 }
